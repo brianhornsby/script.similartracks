368936f5bc720f921cac4852967988c46b645b62
Brian Hornsby <bri.hornsby@gmail.com>
2013-06-22 12:08:25 +1000

    More tidy up of code.

--- default.py
+++ default.py
@@ -37,10 +37,29 @@ else:
 _addonid = 'script.similartracks'
 _settings = xbmcsettings.XBMCSettings(_addonid, sys.argv)
 
+# Get addon information and settings.
+_addonname = _settings.get_name()
+_version = _settings.get_version()
 _maxcount = _settings['maxcount']
-_recursive = (_settings['recursive'] == 'true')
+if not _maxcount:
+    _maxcount = -1
 _runinbackground = (_settings['background'] == 'true')
-_playlisttracks = []
+
+
+def log_debug(msg):
+    if _settings['debug'] == 'true':
+        print '%s: DEBUG: %s' % (_addonid, msg)
+
+
+def log_error(msg):
+    print '%s: ERROR: %s' % (_addonid, msg)
+
+log_debug('Addon Id:   [%s]' % (_addonid))
+log_debug('Addon Name: [%s]' % (_addonname))
+log_debug('Version:    [%s]' % (_version))
+
+log_debug('maxcount: %d' % _maxcount)
+log_debug('runinbackground: %d' % _runinbackground)
 
 if not _runinbackground:
     pDialog = xbmcgui.DialogProgress()
@@ -53,54 +72,40 @@ def display_notification(header, message):
     utils.notification(header, message, image=image)
 
 
-def get_next_track_to_add(prev_artist):
-    index = 0
-    for track in _playlisttracks:
-        if track["artist"] != prev_artist:
-            if track["added"] == False:
-                return index
-        index = index + 1
-    index = 0
-    for track in _playlisttracks:
-        if track["added"] == False:
-            return index
-        index = index + 1
-    return -1
-
-
-def get_track_index_in_playlist(filename):
-    index = 0
-    for track in _playlisttracks:
-        if track["filename"] == filename:
-            return index
-        index = index + 1
-    return 0
-
-
 def get_lastfm_similar_tracks(artist, track):
     base_url = 'http://ws.audioscrobbler.com/2.0/?'
     params = {'method': 'track.getsimilar', 'artist': artist, 'track':
-              track, 'api_key': '5da513b631898f5372a5e5f863651212', 'format': 'json'}
+              track, 'api_key': '5da513b631898f5372a5e5f863651212', 'format': 'json',
+              'autocorrect': 1}
     url = '%s%s' % (base_url, urllib.urlencode(params))
-    f = urlopen(url)
+    log_debug('Last.fm URL: %s' % url)
+
+    try:
+        f = urlopen(url)
+    except URLError as exception:
+        log_error(exception)
+        utils.ok(_settings.get_string(1000), _settings.get_string(3007))
+        return []
+
     json_query = unicode(f.read(), 'utf-8', errors='ignore')
     f.close()
     json_response = simplejson.loads(json_query)
     lastfmtracks = []
     for track in json_response['similartracks']['track']:
-        lastfmtracks.append({'title': track['name'], 'artist': track['artist']['name']})
+        lastfmtracks.append({'title': track[
+                            'name'], 'artist': track['artist']['name']})
     return lastfmtracks
 
 
 def get_similar_tracks(artist, title):
+    log_debug('Looking for similar tracks to %s - %s' % (artist, title))
     lastfmtracks = get_lastfm_similar_tracks(artist, title)
     message = (_settings.get_string(3003) % (len(lastfmtracks)))
     if _runinbackground:
         display_notification(_settings.get_string(1000), message)
     else:
         pDialog.update(75, _settings.get_string(3002), message)
-    count = 0
-    no = 1
+    log_debug('Last.fm returned %d similar tracks' % (len(lastfmtracks)))
 
     json_query = xbmc.executeJSONRPC(
         '{"jsonrpc": "2.0", "method": "AudioLibrary.GetArtists", "params": {"properties": [], "sort": { "method": "label" } }, "id": 1}')
@@ -113,10 +118,11 @@ def get_similar_tracks(artist, title):
             artists.append(
                 {'artist': artist['artist'], 'id': artist['artistid']})
 
+    count = 0
+    playlisttracks = []
     for track in lastfmtracks:
         tracktitle = track['title'].encode('ascii', 'ignore')
         trackartist = track['artist'].encode('ascii', 'ignore')
-        no = no + 1
         artistid = None
         for artist in artists:
             if 'artist' in artist and artist['artist'].encode('ascii', 'ignore') == trackartist:
@@ -124,58 +130,85 @@ def get_similar_tracks(artist, title):
                 break
         if artistid:
             json_query = xbmc.executeJSONRPC(
-                '{"jsonrpc": "2.0", "method": "AudioLibrary.GetSongs", "params": {"properties": ["title", "artist", "album", "genre", "track", "file", "thumbnail"], "sort": { "method": "label" },  "filter": {"artistid": %s} }, "id": 1}' % artistid)
+                '{"jsonrpc": "2.0", "method": "AudioLibrary.GetSongs", "params": {"properties": ["title", "artist"], "sort": { "method": "label" },  "filter": {"artistid": %s} }, "id": 1}' % artistid)
             json_query = unicode(json_query, 'utf-8', errors='ignore')
             json_response = simplejson.loads(json_query)
             if (json_response['result'] is not None) and ('songs' in json_response['result']):
                 for song in json_response['result']['songs']:
                     if 'title' in song and song['title'] == tracktitle:
-                        _playlisttracks.append({'songid': song[
-                                               'songid'], 'artist': trackartist, 'added': False})
+                        playlisttracks.append({'songid': song[
+                                               'songid'], 'artist': song['artist'], 'title': song['title'], 'added': False})
                         count = count + 1
                         if not _runinbackground:
                             pDialog.update(85, _settings.get_string(3004) % ('%s - %s' % (
                                 trackartist, tracktitle)), _settings.get_string(3001) % count)
                         break
-    return count
+
+    return (count, playlisttracks)
+
+
+def get_next_track_to_add(previous_artist, playlisttracks):
+    index = 0
+    for track in playlisttracks:
+        if track['artist'][0] != previous_artist:
+            if track['added'] is False:
+                return index
+        index = index + 1
+    index = 0
+    for track in playlisttracks:
+        if track['added'] is False:
+            return index
+        index = index + 1
+    return -1
+
+
+def add_tracks_to_playlist(artist, playlisttracks):
+    index = 0
+    previous_artist = artist
+    while index < _maxcount or _maxcount < 0:
+        i = get_next_track_to_add(previous_artist, playlisttracks)
+        if i == -1:
+            break
+        previous_artist = playlisttracks[i]['artist']
+        json_query = xbmc.executeJSONRPC(
+            '{ "jsonrpc": "2.0", "method": "Playlist.Add", "params": { "playlistid": 0, "item": { "songid": %d } }, "id": 1 }' % playlisttracks[i]['songid'])
+        json_query = unicode(json_query, 'utf-8', errors='ignore')
+        json_response = simplejson.loads(json_query)
+        playlisttracks[i]['added'] = True
+        index = index + 1
+    return (index, playlisttracks)
+
 
 if xbmc.Player().isPlayingAudio():
     tag = xbmc.Player().getMusicInfoTag()
+    artist = tag.getArtist()
+    title = tag.getTitle()
     playlist = xbmc.PlayList(0)
-    currenttrackpos = playlist.getposition()
-    currenttrack = playlist[currenttrackpos].getfilename()
-    message = (_settings.get_string(3005) % (tag.getArtist().decode(
-        'utf-8', 'ignore'), tag.getTitle().decode('utf-8', 'ignore')))
-    if _runinbackground:
-        display_notification(_settings.get_string(1000), message)
-    else:
-        pDialog.update(25, message)
-    count = get_similar_tracks(tag.getArtist(), tag.getTitle())
-    index = 0
-    if count > 0:
-        trackpos = currenttrackpos + 1
-        while xbmc.PlayList(0).size() > trackpos:
-            xbmc.PlayList(0).remove(xbmc.PlayList(0)[trackpos].getfilename())
-        index = 0
-        previous_artist = tag.getArtist()
-        while index < _maxcount:
-            i = get_next_track_to_add(previous_artist)
-            if i == -1:
-                break
-            previous_artist = _playlisttracks[i]['artist']
-            json_query = xbmc.executeJSONRPC(
-                '{ "jsonrpc": "2.0", "method": "Playlist.Add", "params": { "playlistid": 0, "item": { "songid": %d } }, "id": 1 }' % _playlisttracks[i]['songid'])
-            json_query = unicode(json_query, 'utf-8', errors='ignore')
-            json_response = simplejson.loads(json_query)
-            _playlisttracks[i]['added'] = True
-            index = index + 1
-    if not _runinbackground:
-        pDialog.close()
+    currenttrackpos = playlist.getposition() + 1
+    if currenttrackpos <= len(playlist):
+        if _runinbackground:
+            display_notification(_settings.get_string(1000), _settings.get_string(4000) % (artist.decode('utf-8', 'ignore'), title.decode('utf-8', 'ignore')))
+        else:
+            pDialog.update(25, _settings.get_string(3005), '%s - %s' % (artist.decode('utf-8', 'ignore'), title.decode('utf-8', 'ignore')))
 
-    message = (_settings.get_string(3006) % (index, tag.getArtist().decode(
-        'utf-8', 'ignore'), tag.getTitle().decode('utf-8', 'ignore')))
-    if _runinbackground:
-        display_notification(_settings.get_string(1000), message)
+        count, playlisttracks = get_similar_tracks(artist, title)
+        log_debug('Found %d similar tracks in XBMC library' % count)
+
+        index = 0
+        if count > 0:
+            while xbmc.PlayList(0).size() > currenttrackpos:
+                xbmc.PlayList(0).remove(xbmc.PlayList(0)[currenttrackpos].getfilename())
+            index, playlisttracks = add_tracks_to_playlist(artist, playlisttracks)
+
+        if not _runinbackground:
+            pDialog.close()
+
+        log_debug('Added %d songs to playlist' % index)
+        if _runinbackground:
+            display_notification(_settings.get_string(1000), _settings.get_string(4001) % (index, artist.decode('utf-8', 'ignore'), title.decode('utf-8', 'ignore')))
+        else:
+            utils.ok(_settings.get_string(1000), _settings.get_string(3006) % index, '%s - %s' % (artist.decode('utf-8', 'ignore'), title.decode('utf-8', 'ignore')))
     else:
-        dialog = xbmcgui.Dialog()
-        ok = dialog.ok(_settings.get_string(1000), message)
+        log_debug('Unable to get currently playing track')
+else:
+    utils.ok(_settings.get_string(1000), _settings.get_string(3008))
